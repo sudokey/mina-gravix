c6931147056b5a03ebcf829098569096b871f359
Pavel Ivanov <sudokey@gmail.com>
2023-11-14 11:39:03 +0300

    Table

--- src/pages/TradePage/components/Positions/index.tsx
+++ src/pages/TradePage/components/Positions/index.tsx
@@ -2,8 +2,61 @@ import * as React from 'react'
 
 import styles from './index.module.scss'
 
+import { Text } from '@/components/Text'
+
 export const Positions = () => (
     <div className={styles.root}>
-        Positions
+        <div className={styles.head}>
+            <div className={styles.row}>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        ID
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Type
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Net value
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Size
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Collateral
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Mark price
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Open price
+                    </Text>
+                </div>
+                <div className={styles.cell}>
+                    <Text color="gray" size="s" as="div">
+                        Liq. price
+                    </Text>
+                </div>
+                <div className={styles.cell} />
+            </div>
+        </div>
+        <div className={styles.body}>
+            <div className={styles.message}>
+                <Text color="gray">
+                    No data
+                </Text>
+            </div>
+        </div>
     </div>
 )
